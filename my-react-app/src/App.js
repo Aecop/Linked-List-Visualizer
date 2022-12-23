c4c9abd9f6d5ea02baa1ce311008d089ce898dba
Aecop <joonruiz08@gmail.com>
2022-12-22 20:33:43 -0800

    added btn feature and method to run when button is clicked

--- my-react-app/src/App.js
+++ my-react-app/src/App.js
@@ -1,6 +1,7 @@
 import React from "react";
 import logo from './logo.svg';
 import './App.css';
+import './linkedlistVisual/linkedlistVisual.css'
 
 import NewComponent from "./linkedlistVisual/NewComponent";
 import LinkedlistVisual from "./linkedlistVisual/linkedlist";
@@ -8,9 +9,6 @@ import LinkedlistVisual from "./linkedlistVisual/linkedlist";
 function App() {
   return (
     <div className="App">
-        <div className={'btnHolder'}>
-
-        </div>
         <div className={'app-visual'}>
             <NewComponent/>
             <LinkedlistVisual>
